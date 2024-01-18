0ed9d4304cd93bf8bbda5605accb0b9ed678f4ee
Checker5 <157010146+Checker5@users.noreply.github.com>
2024-01-18 12:57:48 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,14 @@
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
 import os
-
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-SUDO = int(os.environ.get("SUDO", ""))
-HEROKU = os.environ.get("HEROKU", "")
-APP_URL = "https://"+ HEROKU +".herokuapp.com/" + BOT_TOKEN
+APP_ID = os.environ.get("APP_ID")
+APP_HASH = os.environ.get("APP_HASH")
+BOT_USERNAME = os.environ.get("BOT_USERNAME")
+session = os.environ.get("TERMUX")
+SESSION = os.environ.get("TERMUX")
+token = os.environ.get("TOKEN")
+sython = TelegramClient(StringSession(session), APP_ID, APP_HASH)
+bot = TelegramClient("bot", APP_ID, APP_HASH).start(bot_token=token)
+ispay = ['yes']
+ispay2 = ['yes']
+bot.start()
