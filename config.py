975282352bceb60336038ea2afd2e8fdd7bc775a
Checker5 <157010146+Checker5@users.noreply.github.com>
2024-01-18 13:00:18 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,6 @@ import os
 APP_ID = os.environ.get("APP_ID")
 APP_HASH = os.environ.get("APP_HASH")
 BOT_USERNAME = os.environ.get("BOT_USERNAME")
-session = os.environ.get("TERMUX")
-SESSION = os.environ.get("TERMUX")
 token = os.environ.get("TOKEN")
 sython = TelegramClient(StringSession(session), APP_ID, APP_HASH)
 bot = TelegramClient("bot", APP_ID, APP_HASH).start(bot_token=token)
