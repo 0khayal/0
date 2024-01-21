51240048d25a41735fe0db93013825da2b8366c3
Checker5 <157010146+Checker5@users.noreply.github.com>
2024-01-21 16:43:23 +0200

    Update checktele.py

--- checktele.py
+++ checktele.py
@@ -46,13 +46,13 @@ def gen_user(choice):
         c = random.choices(a)
         d = random.choices(b)
         s = random.choices(e)
-        f = [c[0], "_", d[0], "_", s[0]]
+        f = [c[0], d[0], "_", d[0], c[0]
         username = ''.join(f)
         if username in banned[0]:
             c = random.choices(a)
             d = random.choices(b)
             s = random.choices(e)
-            f = [c[0], "_", d[0], "_", s[0]]
+            f = [c[0], d[0], "_", d[0], c[0]
             username = ''.join(f)
         else:
             pass
